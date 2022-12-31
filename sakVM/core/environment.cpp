ce653247c49e5e10c5b8ad1e18782946b580e50c
PowerAngelXD <692732163@qq.com>
2022-12-31 10:47:57 +0800

    Update: Methods implement

--- sakVM/core/environment.cpp
+++ sakVM/core/environment.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "environment.h"
+
+using namespace sakVM_core;
+
+Environment::Environment(int ln, int col): runtime(), line(ln), column(col) {}
+
+void Environment::push(storage::Val val) {
+    runtime.push(val);
+}
+storage::Val Environment::peek() { return runtime.top(); }
+storage::Val Environment::pop() {
+    storage::Val ret = runtime.top();
+    runtime.pop();
+    return ret;
+}
+
+bool Environment::findIdentifier(const std::string& ident) { return space.findMember(ident); }
+storage::Val & Environment::getVal(const std::string& ident, int ln, int col) { return space.getVal(ident, ln, col); }
+void Environment::deleteVal(const std::string& ident, int ln, int col) { space.deleteMember(ident, ln, col); }
+
+template<typename ValType>
+void Environment::createValue(std::string ident, ValType val, int ln, int col) { space.createMember(ident, val, ln, col); }
+
+template<typename ValType>
+void Environment::createValueWithType(std::string ident, ValType val, type::Type type, int ln, int col) {
+    space.createMemberWithType(ident, val, type, ln, col);
+}
--- sakVM/core/environment.h
+++ sakVM/core/environment.h
@@ -9,34 +9,46 @@
 #include <stack>
 
 namespace sakVM_core {
+    // This class represents an environment in which a program can be executed.
+    // An environment consists of a space in which variables and their values are stored,
+    // a runtime stack used to store temporary values during program execution,
+    // and information about the current line and column in the source code.
     class Environment {
-        // the space stores symbol tables of different levels and corresponding values
+        // The space in which variables and their values are stored.
         storage::Space space;
-        /*
-         * The value stack of the runtime VM must have only one value or be empty at the end of the run;
-         * Some values in the value stack cannot be accessed, but they play a very important role in the VM level
-        */
+        // A stack used to store temporary values during program execution.
         std::stack<storage::Val> runtime;
-
-        int line, column;
+        // The current line in the source code.
+        int line;
+        // The current column in the source code.
+        int column;
     public:
+        // Constructs a new environment with the given line and column information.
         Environment(int ln, int col);
 
-        void push(storage::Val);
+        // Pushes a value onto the runtime stack.
+        void push(storage::Val val);
+        // Returns the top value on the runtime stack without removing it.
         storage::Val peek();
+        // Removes and returns the top value on the runtime stack.
         storage::Val pop();
 
-        bool findIdentifier(std::string ident);
-        storage::Val& getVal(std::string ident);
-        void deleteVal(std::string ident);
+        // Returns true if the environment contains a variable with the given identifier.
+        bool findIdentifier(const std::string& ident);
+        // Returns a reference to the value of the variable with the given identifier, or throws an error if it does not exist.
+        storage::Val &getVal(const std::string& ident, int ln = -1, int col = -1);
+        // Deletes the variable with the given identifier, or throws an error if it does not exist.
+        void deleteVal(const std::string& ident, int ln, int col);
 
+        // Creates a new variable with the given identifier and value, and the default type.
         template<typename ValType>
-        void createValue(std::string ident, ValType val);
+        void createValue(std::string ident, ValType val, int ln, int col);
 
+        // Creates a new variable with the given identifier, value, and type.
         template<typename ValType>
-        void createValueWithType(std::string ident, ValType val, type::Type type);
-
+        void createValueWithType(std::string ident, ValType val, type::Type type, int ln, int col);
     };
+
 }
 
 #endif //SAKORA_ENVIRONMENT_H
