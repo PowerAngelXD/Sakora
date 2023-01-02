0a595db5a54c3df3849144fa7fa0f1b04cf8640f
PowerAngelXD <692732163@qq.com>
2023-01-02 11:22:39 +0800

    Update: VM implements, now it can works!

--- sakVM/vm.cpp
+++ sakVM/vm.cpp
@@ -3,3 +3,77 @@
 //
 
 #include "vm.h"
+
+#include <utility>
+
+// VM
+sakVM::sakVM(std::vector<std::string> cp, int ln, int col): env(std::move(cp), ln, col) {};
+
+void sakVM::vm_init(std::vector<std::string> cp, int ln, int col) {
+    env = sakVM_core::Environment(std::move(cp), ln, col);
+}
+
+void sakVM::vm_run(size_t layer) {
+    auto cur_queue = threads[layer];
+    for (auto code: cur_queue) {
+        switch (code.kind) {
+            case visitor::push_float: ins_push_float(code); break;
+            case visitor::push_double: ins_push_double(code); break;
+            case visitor::push_i16: ins_push_i16(code); break;
+            case visitor::push_i32: ins_push_i32(code); break;
+            case visitor::push_i64: ins_push_i64(code); break;
+            case visitor::push_bool: ins_push_bool(code); break;
+            case visitor::push_str: ins_push_str(code); break;
+            case visitor::push_iden: ins_push_iden(code); break;
+            case visitor::add: ins_add(); break;
+            case visitor::sub: ins_sub(); break;
+            case visitor::mul: ins_mul(); break;
+            case visitor::div: ins_div(); break;
+            case visitor::gmem: ins_gmem(); break;
+            case visitor::mod: ins_mod(); break;
+        }
+    }
+}
+
+// Code set
+void sakVM::ins_push_i16(visitor::Code code) { env.push(static_cast<short>(code.val)); }
+void sakVM::ins_push_i32(visitor::Code code) { env.push(static_cast<int>(code.val)); }
+void sakVM::ins_push_i64(visitor::Code code) { env.push(static_cast<long long>(code.val)); }
+void sakVM::ins_push_float(visitor::Code code) { env.push(static_cast<float>(code.val)); }
+void sakVM::ins_push_double(visitor::Code code) { env.push(static_cast<double>(code.val)); }
+void sakVM::ins_push_bool(visitor::Code code) { env.push(static_cast<bool>(float_equ(code.val, code.val)))); }
+void sakVM::ins_push_str(visitor::Code code) {
+    auto str = env.getConstant(static_cast<size_t>(static_cast<int>(code.val)));
+    env.push(str);
+}
+void sakVM::ins_push_iden(visitor::Code code) {
+    std::cout<<code.val<<std::endl;
+}
+void sakVM::ins_add() {
+    auto right = env.pop();
+    auto left = env.pop();
+    env.push(left + right);
+}
+void sakVM::ins_sub() {
+    auto right = env.pop();
+    auto left = env.pop();
+    env.push(left - right);
+}
+void sakVM::ins_mul() {
+    auto right = env.pop();
+    auto left = env.pop();
+    env.push(left * right);
+}
+void sakVM::ins_div() {
+    auto right = env.pop();
+    auto left = env.pop();
+    env.push(left / right);
+}
+void sakVM::ins_mod() {
+    auto right = env.pop();
+    auto left = env.pop();
+    env.push(left % right);
+}
+void sakVM::ins_gmem() {
+
+}
--- sakVM/vm.h
+++ sakVM/vm.h
@@ -9,26 +9,40 @@
 #include "core/tool.h"
 #include "core/codequeue.h"
 
-namespace sakVM {
+struct sakVM {
     /**
      * Naming conventions:
      * vm_xxx: Operation on VM
      */
-    static std::string vm_info = "Sakora VM (Version: 1.0)";
+    std::string vm_info = "Sakora VM (Version: 1.0)";
     // VM code queues
-    static std::vector<sakVM_core::CodeQueue> threads;
+    std::vector<sakVM_core::CodeQueue> threads;
     // Environment of the VM
-    static sakVM_core::Environment env;
+    sakVM_core::Environment env;
 
-    // VM instruction set
-    namespace sakVM_code_set {
-
-    }
+    sakVM(std::vector<std::string> cp, int ln, int col);
 
     // Initialization operation for VM
     void vm_init(std::vector<std::string> cp, int ln, int col);
     // Run the code queue at the specified level
     void vm_run(size_t layer);
-}
+    // VM instruction set
+    void ins_push_i16(visitor::Code code);
+    void ins_push_i32(visitor::Code code);
+    void ins_push_i64(visitor::Code code);
+    void ins_push_float(visitor::Code code);
+    void ins_push_double(visitor::Code code);
+    void ins_push_bool(visitor::Code code);
+    void ins_push_str(visitor::Code code);
+    void ins_push_iden(visitor::Code code);
+    void ins_add();
+    void ins_sub();
+    void ins_mul();
+    void ins_div();
+    void ins_gmem();
+    void ins_mod();
+
+};
+
 
 #endif //SAKORA_VM_H
