cf693e82ad343fa012610820d176b6b10e0523dd
PowerAngelXD <692732163@qq.com>
2023-01-15 22:12:04 +0800

    Change: new test

--- main.cpp
+++ main.cpp
@@ -3,21 +3,21 @@
 
 
 int main() {
-    lexer::Lexer lex("3 != 4 && 1 == 2");
+    lexer::Lexer lex("3+2");
     lex.generate();
 
     parser::Parser p(lex.out);
     try {
-        auto node = p.parseBooleanExpressionNode();
-        visitor::Visitor vr(node);
-        vr.visitBooleanExpression(node);
+        auto node = p.parseAddExprNode();
+        //visitor::Visitor vr(node);
+        //vr.visitAddExpression(node);
 
-        sakVM vm(vr.constantPool, node[parser::Marker::head][0].token.line, node[parser::Marker::head][0].token.column);
-        vm.threads.push_back(vr.out);
-        vm.vm_run(0);
+        //sakVM vm(vr.constantPool, node[parser::Marker::head][0].token.line, node[parser::Marker::head][0].token.column);
+        //vm.threads.push_back(vr.out);
+        //vm.vm_run(0);
 
-        auto result = vm.env.pop();
-        std::cout<<*(bool*)(result.val())<<std::endl;
+        //auto result = vm.env.pop();
+        std::cout<<node->head->head->head->head->factor->token->content<<std::endl;
     }
     catch (parser_error::UnexpectedTokenError& e) {
         e.printError();
