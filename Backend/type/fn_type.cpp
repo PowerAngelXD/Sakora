5e99eb18414ceafbe4e46be237ee00e6e7295c38
PowerAngelXD <692732163@qq.com>
2023-01-31 17:23:36 +0800

    New files: New structure of types

---
+++ Backend/type/fn_type.cpp
@@ -0,0 +1,5 @@
+//
+// Created by BALL on 2023/1/26.
+//
+
+#include "fn_type.h"
---
+++ Backend/type/fn_type.h
@@ -0,0 +1,8 @@
+//
+// Created by BALL on 2023/1/26.
+//
+
+#ifndef SAKORA_FN_TYPE_H
+#define SAKORA_FN_TYPE_H
+
+#endif //SAKORA_FN_TYPE_H
