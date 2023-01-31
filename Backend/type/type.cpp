e73834cda08f00238db8e71c955016bf5a1b980e
PowerAngelXD <692732163@qq.com>
2023-01-31 17:51:24 +0800

    Change: move contents to basic_type

--- Backend/type/type.cpp
+++ Backend/type/type.cpp
@@ -1,11 +1,5 @@
 //
-// Created by BALL on 2022/12/25.
+// Created by BALL on 2023/1/26.
 //
 
 #include "type.h"
-
-using namespace type;
-
-Type::Type(BasicType b): basic(b), st(Structure::Normal) {}
-Type::Type(BasicType b, Structure s): basic(b), st(s) {}
-Type::Type(BasicType b, Structure s, StorageKind stok): basic(b), st(s), stk(stok) {}
--- Backend/type/type.h
+++ Backend/type/type.h
@@ -1,53 +1,8 @@
 //
-// Created by BALL on 2022/12/25.
+// Created by BALL on 2023/1/26.
 //
 
 #ifndef SAKORA_TYPE_H
 #define SAKORA_TYPE_H
-
-#include "../visitor/visitor.h"
-
-namespace type {
-    // Enumeration of basic data types
-    enum BasicType {
-        Int16,   // 16-bit integer
-        Int32,   // 32-bit integer
-        Int64,   // 64-bit integer
-        Float,   // single precision floating point number
-        Double,  // double precision floating point number
-        Boolean, // boolean value
-        String,  // string
-        Typeid,  // typeid
-        Object   // object
-    };
-
-    enum StorageKind {
-        Ref, // indicating value is stored as a reference
-        Val // indicating value is stored as a value
-    };
-
-    // Enumeration representing the structure of an object
-    enum Structure {
-        Normal, // normal value
-        List,   // list
-        Struct, // struct
-        StructArray, // struct array
-        Tuple   // tuple
-    };
-
-    // Struct representing a data type
-    struct Type {
-        BasicType basic;       // basic type of the object
-        Structure st;          // structure of the object
-        StorageKind stk;       // storage of the object
-
-        // Constructors
-        Type() = default;                      // default constructor
-        Type(BasicType b);                     // construct with basic type
-        Type(BasicType b, Structure s);        // construct with basic type, ownership, modifier, and structure
-        Type(BasicType b, Structure s, StorageKind stok);
-    };
-
-}
 
 #endif //SAKORA_TYPE_H
