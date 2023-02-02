0054f38fda2dc93e15306254343323471b44155a
PowerAngelXD <692732163@qq.com>
2023-02-02 21:38:28 +0800

    Update: new type system

--- Backend/type/type.cpp
+++ Backend/type/type.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "type.h"
+
+type::Type::Type(UnitType unit_type) {
+    type_content.push_back({nullptr, new UnitType(unit_type)});
+    head = type_content[0];
+}
+type::Type::Type(std::initializer_list<TSOption> TS_list) {
+    for (auto element: TS_list) {
+        type_content.push_back(element);
+    }
+    head = type_content[0];
+}
+
+bool type::Type::isStructure() const {
+    return head.s_kind != nullptr;
+}
+
+bool type::Type::operator== (Type t) {
+    for(size_t i = 0; i < this->type_content.size(); i ++) {
+        if (!(this->type_content[i].unit_type == t.type_content[i].unit_type &&
+            this->type_content[i].s_kind == t.type_content[i].s_kind))
+            return false;
+    }
+    return true;
+}
+bool type::Type::operator!= (Type t) {
+    return !this->operator==(t);
+}
--- Backend/type/type.h
+++ Backend/type/type.h
@@ -5,4 +5,35 @@
 #ifndef SAKORA_TYPE_H
 #define SAKORA_TYPE_H
 
+#include "basic_type.h"
+
+namespace type {
+    enum StructureKind {
+        Struct,
+        Array,
+        VarArray,
+        Impl,
+        Fn,
+    };
+
+    // Type-Structure Option
+    struct TSOption {
+        StructureKind* s_kind = nullptr;
+        UnitType* unit_type = nullptr;
+    };
+
+    struct Type {
+        std::vector<TSOption> type_content;
+        TSOption head;
+
+        Type()=default;
+        Type(UnitType unit_type);
+        Type(std::initializer_list<TSOption> TS_list);
+
+        [[nodiscard]] bool isStructure() const;
+        bool operator== (Type t);
+        bool operator!= (Type t);
+    };
+}
+
 #endif //SAKORA_TYPE_H
