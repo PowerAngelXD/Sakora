20a51028c8b8c2d68aab210965f9e36d6cd382be
PowerAngelXD <692732163@qq.com>
2023-02-02 21:38:51 +0800

    Change

--- Backend/scope/value.h
+++ Backend/scope/value.h
@@ -5,13 +5,24 @@
 #ifndef SAKORA_VALUE_H
 #define SAKORA_VALUE_H
 
-#include "../type/basic_type.h"
+#include "../type/type.h"
+#include "../../error/storage_error.h"
 #include <cmath>
+#include <float.h>
 #include <cstring>
 
 #define float_equ(a,b) ((fabs((a) - (b)) < (1e-8))
+#define INT(val) *(long long*)val
+#define DECI(val) *(double*)val
+#define STR(val) *(std::string*)val
+#define BOOL(val) *(bool*)val
+#define TYPEID(val) *(type::Type*)val
+
 
 namespace storage {
+    // Check whether two floating point numbers are equal
+    bool doubleEqual(double d1, double d2);
+
     // Val class represents a value in the program, which could be a basic type or a user-defined type
     class Val {
         // Pointer to the value stored in this Val object
@@ -19,17 +30,15 @@ namespace storage {
         // Size of the value stored in this Val object
         size_t val_size = 0;
         // UnitType of the value stored in this Val object
-        type::UnitType val_type;
+        type::Type val_type;
 
+        int line, column;
     public:
         // Default constructor that creates an empty Val object with no value
         Val()=default;
 
         // Constructors to create a Val object with a basic type value of the given type
-        Val(int v);
-        Val(long v);
         Val(long long v);
-        Val(float v);
         Val(double v);
         Val(bool v);
         Val(std::string v);
@@ -37,7 +46,7 @@ namespace storage {
 
         // Constructor to create a Val object with a value of the given type and a polonger to the value
         template<typename T>
-        Val(T v, type::UnitType t);
+        Val(T v, type::Type t);
 
         // Operators to perform arithmetic and comparison operations on Val objects
         Val operator+ (Val v);
@@ -55,10 +64,12 @@ namespace storage {
         Val operator &&(Val v);
         Val operator! ();
 
+        // Set value line and column
+        void setLnCol(int ln, int col);
         // Returns the basic type of the value stored in this Val object (e.g. long, Float, Bool)
         type::BasicType getBasicType() const;
         // Returns the type of the value stored in this Val object (could be a basic type or a user-defined type)
-        type::UnitType getType() const;
+        type::Type getType() const;
         // Returns a pointer to the value stored in this Val object
         void* val();
         // Deallocates the memory used by the value stored in this Val object
