7b9c03a99ae3d51b214864bd4ac476e53ac4b6d0
PowerAngelXD <692732163@qq.com>
2023-02-03 11:29:28 +0800

    Update: repl

--- main.cpp
+++ main.cpp
@@ -1,25 +1,7 @@
-#include "Backend/scope/value.h"
-#include "sakVM/vm.h"
+#include "sakConsole/commands.h"
 
 
 int main() {
-    lexer::Lexer lex("3+2");
-    lex.generate();
-
-    parser::Parser p(lex.out);
-    try {
-        auto node = p.parseAddExprNode();
-        visitor::Visitor vr;
-        vr.visitAddExpression(node);
-
-        sakVM vm(vr.constantPool, vr.out[0].ln, vr.out[0].col);
-        vm.threads.push_back(vr.out);
-        vm.vm_run(0);
-
-        std::cout<<*(long long*)vm.env.pop().val()<<std::endl;
-    }
-    catch (parser_error::UnexpectedTokenError& e) {
-        e.printError();
-    }
+    sakConsole::cmdRepl();
     return 0;
 }
