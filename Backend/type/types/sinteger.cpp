905e8aa3d96c4e08c23abf5b3a7cc401efd6134b
PowerAngelXD <692732163@qq.com>
2023-03-09 13:34:17 +0800

    Update: type file of integer

--- Backend/type/types/sinteger.cpp
+++ Backend/type/types/sinteger.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "sinteger.h"
+
+type::IntKind type::SInteger::getKind() {
+    return ikind;
+}
+
+int type::SInteger::getI32() {
+    return 0;
+}
+
+long long type::SInteger::getI64() {
+    return 0;
+}
