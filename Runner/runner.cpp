4cba5bc5d7abcd5f3cfdd8fc04db0f7d0557dc1a
PowerAngelXD <692732163@qq.com>
2023-04-13 12:18:10 +0800

    Update: Runner files

---
+++ Runner/runner.cpp
@@ -0,0 +1,5 @@
+//
+// Created by BALL on 2023/4/13.
+//
+
+#include "runner.h"
---
+++ Runner/runner.h
@@ -0,0 +1,17 @@
+//
+// Created by BALL on 2023/4/13.
+//
+
+#ifndef SAKORA_RUNNER_H
+#define SAKORA_RUNNER_H
+
+#include "../Backend/sakCalc/calc.h"
+
+namespace runner {
+    class Runner {
+        sakCalc calc;
+
+    };
+}
+
+#endif //SAKORA_RUNNER_H
